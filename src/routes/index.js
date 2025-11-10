5d86c9ae8b38d3bc4afd5e03a148a8861deef3ec
hugh.huynh <trihuynh030697@gmail.com>
2025-11-10 20:52:05 +0700

    test ci/cd

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/v1/api', require('./access'));
+router.use('/v1/api', (req, res) => {
+    return res.status(200).json({ message: 'Welcome API' });
+});
 
 module.exports = router;
