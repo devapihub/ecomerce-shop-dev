0eb8376d6a2f9c706506ed267694123a1edf2e55
hugh.huynh <trihuynh030697@gmail.com>
2025-11-20 18:16:15 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,11 +13,13 @@ router.use(permission('0000'));
 
 router.use('/v1/api', require('./access'));
 
+/*
 // authentication //
-//router.use(authentication)
+router.use(authentication)
 //////////////////////////
 
 router.use('/v1/api', require('./shop'));
 router.use('/v1/api/product', require('./product'));
+*/
 
 module.exports = router;
