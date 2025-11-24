1c37e7cf215c282492f9cb85825e05692bb817e9
hugh.huynh <trihuynh030697@gmail.com>
2025-11-24 21:14:07 +0700

    revert login by email

--- src/models/shop.model.js
+++ src/models/shop.model.js
@@ -6,22 +6,17 @@ const COLLECTION_NAME = "shops";
 
 const ShopSchema = new Schema({
     name: {type: String, required: true, maxLength: 150},
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
+    email: {type: String, unique: true, trim: true},
+    password: {type: String, required: true},
     status: {type: String, enum: ['active', 'inactive'], default: 'inactive'},
+    verify: {type: Schema.Types.Boolean, default: false},
     role: {
-        type: [String],
+        type: Array,
         default: []
-    },
-    avatar: {type: String, default: ''}
+    }
 }, {
     timestamps: true,
     collection: COLLECTION_NAME
 });
-
-ShopSchema.index({owner: 1});
 
 module.exports = model(DOCUMENT_NAME, ShopSchema);
--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -7,12 +7,7 @@ const router = express.Router();
 const {authentication} = require("../../auth/authUtils");
 
 
-router.post('/shop/send-otp', asyncHandler(accessController.sendOTP));
-router.post('/shop/verify-and-signup', asyncHandler(accessController.verifyAndSignup));
 router.post('/shop/login', asyncHandler(accessController.login));
-router.post('/shop/google', asyncHandler(accessController.googleLogin));
-router.post('/shop/forgot-password', asyncHandler(accessController.forgotPassword));
-router.post('/shop/reset-password', asyncHandler(accessController.resetPassword));
 
 router.post('/shop/logout', authentication, asyncHandler(accessController.logout));
 router.post('/shop/refresh-token', authentication, asyncHandler(accessController.handleRefreshToken));
